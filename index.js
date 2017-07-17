bb284058673a876bd046c4c95a674726c7a5bd4a
Mike McDonald <mikemcd97@gmail.com>
2017-07-17 19:26:12 +0000

    calculator made

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/hello', function (req, res) {
+  if (req.query.name){
+    res.send('Hello,' + " " + req.query.name);
+  }
+  else{
+    res.send('Hello World!');
+  }
 });
 
+ app.get('/calculator/:operation', function (req, res){
+  var num1 = req.query.num1 || 0;
+  var num2 = req.query.num2 || 0;
+  if(req.params.operation === 'add'){
+    res.send({operation: 'add', 
+    firstOperand: num1, 
+    secondOperand: num2, 
+    solution: Number(num1)+Number(num2)});
+  }
+  else if(req.params.operation === 'multiply'){
+    res.send({operation: 'multiply', 
+    firstOperand: num1, 
+    secondOperand: num2, 
+    solution: num1 * num2});
+  }
+ 
+});
 
 
 
